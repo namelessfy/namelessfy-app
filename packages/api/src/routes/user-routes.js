46bc1a9d1c8cb4a398ad09a2adb7a325bb1680f9
Estefanía Cordeiro <estefania.cbrion@gmail.com>
2021-04-14 17:13:38 +0200

    feat: create delete user endpoint

--- packages/api/src/routes/user-routes.js
+++ packages/api/src/routes/user-routes.js
@@ -8,6 +8,7 @@ const userRouter = Router();
 userRouter.post("/sign-up", authMiddleware, userController.signUp);
 userRouter.post("/sign-out", authMiddleware, userController.signOut);
 userRouter.patch("/user/edit", authMiddleware, userController.edit);
+userRouter.delete("/user/delete", authMiddleware, userController.delete);
 
 module.exports = {
   userRouter: userRouter,
