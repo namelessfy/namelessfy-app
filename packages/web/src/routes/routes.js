1a65024bc952b5c4f0296eb8587e59f4cc9ee2bb
soyyogi <raoyogesh78@yahoo.in>
2021-04-21 01:57:49 +0200

    feat: created upload song react route

--- packages/web/src/routes/routes.js
+++ packages/web/src/routes/routes.js
@@ -3,3 +3,4 @@ export const SIGN_UP = "/sign-up";
 export const LOGIN = "/login";
 export const RESET_PASSWORD = "/reset-password";
 export const COMPLETE_SIGNUP = "/complete-signUp";
+export const UPLOAD_SONG = "/upload-song";
