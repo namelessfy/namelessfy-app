4901d0a7753de7d898e48b945350ae15d3ca14c7
soyyogi <raoyogesh78@yahoo.in>
2021-04-21 02:02:02 +0200

    feat: created uploader redux reducer

--- packages/web/src/redux/root-reducer.js
+++ packages/web/src/redux/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import authReducer from "./auth/auth-reducer";
+import UploaderReducer from "./uploader/uploader-reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  uploader: UploaderReducer,
 });
 
 export default rootReducer;
