8985710c584f8eaa34d933e2c616669bfdeba771
soyyogi <raoyogesh78@yahoo.in>
2021-04-21 02:03:41 +0200

    feat: added createTrack api func

--- packages/web/src/api/api.js
+++ packages/web/src/api/api.js
@@ -26,10 +26,20 @@ function makeApi(request = makeRequest()) {
     });
   }
 
+  function createTrack({ body, headers = {} }) {
+    return request({
+      url: "/tracks",
+      requestMethod: "POST",
+      body: body,
+      headers: headers,
+    });
+  }
+
   return {
     signUp: signUp,
     signOut: signOut,
     editUser: editUser,
+    createTrack: createTrack,
   };
 }
 
