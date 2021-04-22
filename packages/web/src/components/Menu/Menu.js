aa3c0f011e4f11f498b7cf137348fa59e689b9b5
Jan Puig <janpuigcapella@gmail.com>
2021-04-22 13:05:34 +0200

    feat: close menu when clicking ot of it

--- packages/web/src/components/Menu/Menu.js
+++ packages/web/src/components/Menu/Menu.js
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 
 import { signOut } from "../../redux/auth/auth-actions";
 
-import { Background } from "./style";
+import { Background, Back } from "./style";
 import { Button } from "../../styles/formStyles";
 
 function SeacrhModal({ show, close }) {
@@ -15,16 +15,18 @@ function SeacrhModal({ show, close }) {
   }
   return (
     <>
-      <Background isShowing={show}>
-        <h2>Menu:</h2>
-        <Button type="button" onClick={close}>
-          close
-        </Button>
-        <br />
-        <Button type="button" onClick={handleSignOut}>
-          Sign Out
-        </Button>
-      </Background>
+      <Back isShowing={show} onClick={close}>
+        <Background isShowing={show}>
+          <h2>Menu:</h2>
+          <Button type="button" onClick={close}>
+            close
+          </Button>
+          <br />
+          <Button type="button" onClick={handleSignOut}>
+            Sign Out
+          </Button>
+        </Background>
+      </Back>
     </>
   );
 }
