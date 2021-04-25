1122b1a6588b0ab335e5bf523e1771168bcc77ff
Jan Puig <janpuigcapella@gmail.com>
2021-04-25 17:19:24 +0200

    fix: remove unnecessary PropTypes code

--- packages/web/src/components/ProtectedRoute/ProtectedRoute.js
+++ packages/web/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
-import PropTypes from "prop-types";
 
 import { authSelector } from "../../redux/auth/auth-selectors";
 import * as ROUTES from "../../routes";
@@ -14,15 +13,5 @@ function ProtectedRoute({ ...props }) {
     <Redirect to={ROUTES.LOGIN} />
   );
 }
-
-ProtectedRoute.propTypes = {
-  path: PropTypes.string.isRequired,
-  component: PropTypes.func.isRequired,
-  exact: PropTypes.bool,
-};
-
-ProtectedRoute.defaultProps = {
-  exact: false,
-};
 
 export default ProtectedRoute;
